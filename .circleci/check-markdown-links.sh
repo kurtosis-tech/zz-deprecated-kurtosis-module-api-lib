a9294eae0c3814fc7f84dfc3ebb652049afdaac0
Leandro Poroli <leandro.poroli@kurtosistech.com>
2021-07-15 17:46:52 -0300

    Check markdown links script added

---
+++ .circleci/check-markdown-links.sh
@@ -0,0 +1,32 @@
+set -euo pipefail
+script_dirpath="$(cd "$(dirname "${0}")" && pwd)"
+root_dirpath="$(dirname "${script_dirpath}")"
+
+# We can use relative syntax to specify kurtosis-core/kurtosis-libs links in the Markdown (e.g. "./kurtosis-core/architecture")
+# because everything gets published to the same docs.kurtosistech.com domain, but we need to expand to the full URL for the Markdown
+# link checker
+config_filepath="$(mktemp)"
+cat << EOF > "${config_filepath}"
+{
+    "replacementPatterns": [
+        {
+            "pattern": "^../kurtosis-core",
+            "replacement": "https://docs.kurtosistech.com/kurtosis-core"
+        },
+        {
+            "pattern": "^../kurtosis-libs",
+            "replacement": "https://docs.kurtosistech.com/kurtosis-libs"
+        }
+    ]
+}
+EOF
+
+# Inspired by https://github.com/open-telemetry/opentelemetry-collector/pull/1156/files/2244e61f4dd0378deffc00d939edf6f800687dcf
+exit_code=0
+for filepath in $(find "${root_dirpath}" -iname '*.md' | sort); do
+    markdown-link-check --config "${config_filepath}" -qv "${filepath}" || exit_code=1
+    # Wait to scan files so that we don't overload github with requests which may result in 429 responses
+    sleep 2
+done
+
+exit "${exit_code}"
