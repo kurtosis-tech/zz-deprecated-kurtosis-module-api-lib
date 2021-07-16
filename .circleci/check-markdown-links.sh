db3449c3ee8ce8a05be834e8c6cdf578949bd542
Leandro Poroli <leandro.poroli@kurtosistech.com>
2021-07-16 09:58:23 -0300

    Update check-markdown-links.sh

--- .circleci/check-markdown-links.sh
+++ .circleci/check-markdown-links.sh
@@ -1,7 +1,16 @@
-set -euo pipefail
-script_dirpath="$(cd "$(dirname "${0}")" && pwd)"
+#!/usr/bin/env bash
+# ^^^^^^^^^^^^^^^^^ this is the most platform-agnostic way to guarantee this script runs with Bash
+# 2021-07-08 WATERMARK, DO NOT REMOVE - This script was generated from the Kurtosis Bash script template
+
+set -euo pipefail   # Bash "strict mode"
+script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 root_dirpath="$(dirname "${script_dirpath}")"
 
+
+
+# ==================================================================================================
+#                                             Main Logic
+# ==================================================================================================
 # We can use relative syntax to specify kurtosis-core/kurtosis-libs links in the Markdown (e.g. "./kurtosis-core/architecture")
 # because everything gets published to the same docs.kurtosistech.com domain, but we need to expand to the full URL for the Markdown
 # link checker
