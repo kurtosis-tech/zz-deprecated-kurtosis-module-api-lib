2412411bbad6937f7e8da0c680670a875d0a8652
Leandro Poroli <leandro.poroli@kurtosistech.com>
2021-07-16 09:55:37 -0300

    Update build script

--- golang/scripts/build.sh
+++ golang/scripts/build.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
-set -euo pipefail
-script_dirpath="$(cd "$(dirname "${0}")" && pwd)"
+# ^^^^^^^^^^^^^^^^^ this is the most platform-agnostic way to guarantee this script runs with Bash
+# 2021-07-08 WATERMARK, DO NOT REMOVE - This script was generated from the Kurtosis Bash script template
+
+set -euo pipefail   # Bash "strict mode"
+script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 lang_root_dirpath="$(dirname "${script_dirpath}")"
 
+
+# ==================================================================================================
+#                                             Main Logic
+# ==================================================================================================
 cd "${lang_root_dirpath}"
 go test ./...
